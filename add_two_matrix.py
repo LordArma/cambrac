0a230b7a7147802569012760508d6e2921d79cce
Arma <arma@jangal.co>
2020-11-27 02:12:56 +0330

    Init

---
+++ add_two_matrix.py
@@ -0,0 +1,93 @@
+import cProfile
+import io
+import json
+import pstats
+import matplotlib.pyplot as plt
+
+
+def add_by(length, is_row=False):
+    A = [[1 for i in range(length)] for j in range(length)]
+    B = [[2 for i in range(length)] for j in range(length)]
+
+    C = [[0 for i in range(length)] for j in range(length)]
+
+    if is_row:
+        for j in range(length):
+            for i in range(length):
+                C[i][j] = A[i][j] + B[i][j]
+    else:
+        for i in range(length):
+            for j in range(length):
+                C[i][j] = A[i][j] + B[i][j]
+
+
+def add_by_row(length):
+    add_by(length, True)
+
+
+def add_by_col(length):
+    add_by(length, False)
+
+
+def simulate(Range, steps=1):
+    rows = []
+    cols = []
+
+    for i in range(1, Range, steps):
+        pr = cProfile.Profile()
+        pr.enable()
+        add_by_row(i)
+        pr.disable()
+        s = io.StringIO()
+        ps = pstats.Stats(pr, stream=s)
+        ps.print_stats()
+        result = s.getvalue().strip()
+        result = result.partition('\n')[0]
+        result = result.split()[4]
+        rows.append(float(result))
+
+    with open('./results/rows.txt', 'w+') as f:
+        f.write(str(rows))
+
+    for i in range(1, Range, steps):
+        pr = cProfile.Profile()
+        pr.enable()
+        add_by_col(i)
+        pr.disable()
+        s = io.StringIO()
+        ps = pstats.Stats(pr, stream=s)
+        ps.print_stats()
+        result = s.getvalue().strip()
+        result = result.partition('\n')[0]
+        result = result.split()[4]
+        cols.append(float(result))
+
+    with open('./results/cols.txt', 'w+') as f:
+        f.write(str(cols))
+
+
+def show_plt(Range=100, is_fresh=False):
+    if is_fresh:
+        simulate(Range)
+
+    test_prefix = ""
+
+    myrows = json.load(open('./results/' + test_prefix + 'rows.txt'))
+    mycols = json.load(open('./results/' + test_prefix + 'cols.txt'))
+
+    fig, ax = plt.subplots()
+
+    line1, = ax.plot(myrows, label="Rows", linewidth=1, linestyle='-')
+    line2, = ax.plot(mycols, label="Cols", linewidth=1, linestyle='-')
+
+    first_legend = ax.legend(handles=[line1], loc='upper right')
+    ax.add_artist(first_legend)
+    ax.legend(handles=[line2], loc='lower right')
+    fig = plt.gcf()
+    fig.canvas.set_window_title('Compare Add Two Matrix by Row & Col')
+    plt.title("Result")
+    plt.show()
+
+
+if __name__ == "__main__":
+    show_plt(500, True)
